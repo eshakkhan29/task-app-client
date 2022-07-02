c2f1e19dc9f64b32261b399e42ac58e87af33472
eshakkhan29 <eshakkhan29@gmail.com>
2022-07-02 13:12:17 +0600

    final commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='h-20 bg-gray-200 fixed bottom-0 w-full'>
+        <footer className='h-20 bg-gray-200 lg:fixed bottom-0 w-full'>
             <div className='h-full flex items-center justify-center'>
                 <p className='text-gray-600'>&copy; All Right Reserve | {new Date().getFullYear()}</p>
             </div>
